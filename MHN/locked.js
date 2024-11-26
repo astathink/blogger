fef81b71ed497c7211234a657eb1349e9140d0d6
astathink <astathink@gmail.com>
2024-11-26 09:23:31 +0700

    Update locked.js

--- MHN/locked.js
+++ MHN/locked.js
@@ -1,7 +1,7 @@
 function detectDevice() {
     var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
     if (!isMobile) {
-      document.body.innerHTML = '<div style="text-align: center;"><img src="https://astathink.github.io/blogger/RushRoyale/logo.png" alt="Logo" style="width: 200px; margin-bottom: 20px;"><h1>Please load this website from a mobile device.</h1></div>';
+      document.body.innerHTML = '<div style="text-align: center;"><img src="https://astathink.github.io/blogger/MHN/logo.png" alt="Logo" style="width: 200px; margin-bottom: 20px;"><h1>Please load this website from a mobile device.</h1></div>';
       document.body.style.textAlign = 'center';
       document.body.style.marginTop = '20%';
       document.body.style.fontFamily = 'Arial, sans-serif';
